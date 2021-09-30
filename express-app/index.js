a6d0f989b4d28f8a7e3f07a0ccf5203ac03ea8e9
ferdowsi rumi <ferdowsirumi@gmail.com>
2021-09-30 15:25:01 -0400

    added files

---
+++ express-app/index.js
@@ -0,0 +1,47 @@
+// import express from 'express';
+// import data from './data/data.json';
+// const app = express();
+// const PORT = 3000;
+
+// //without path
+// app.use(express.static('images'));
+// //with path
+// app.use('/images', express.static('images'));
+
+// app.get('/', (req, res) => 
+// res.send(data)
+// );
+
+
+// app.post('/newitem', (req, res) => 
+// res.send(`a post request with /newItem route on port ${PORT}`)
+// );
+
+// app.put('/item', (req, res) => 
+// res.send(`a put request with /item route on port ${PORT}`)
+// );
+
+// app.delete('/item', (req, res) => 
+// res.send(`a delete request with /item route on port ${PORT}`)
+// );
+
+import express from 'express';
+import path from 'path';
+const app = express();
+ const PORT = 3000;
+
+ app.use(express.static(path.join(__dirname, './static')));
+
+ app.get('/', (req, res) => {
+ res.sendFile(path.join(__dirname, './static/index.html'));
+ });
+ app.get('/index.html', (req, res) => {
+    res.sendFile(path.join(__dirname, './static/index.html'));
+    });
+
+
+app.listen(PORT, ()=>{
+    //console.log(`Your server is running on port ${PORT}`);
+    console.log(`Server is running in http://localhost:${PORT}`);
+    //console.log(data);
+});
